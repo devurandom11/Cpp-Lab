cc81b56a6d6bd9de1dfcdfabf6041691c8090cc4
Mike Crawford <dev.random.87@gmail.com>
2023-03-15 22:41:21 -0700

    Implement swap command

--- DSA-1/Lab 6 - Stacks/SwapCommand.h
+++ DSA-1/Lab 6 - Stacks/SwapCommand.h
@@ -8,11 +8,17 @@
 
 class SwapCommand : public UndoCommand {
  private:
-  // TODO
+  int index1, index2;
+  std::vector<std::string>* sourceVector;
+
  public:
-  // TODO
+  SwapCommand(int index1, int index2, std::vector<std::string>* sourceVector)
+      : index1(index1), index2(index2), sourceVector(sourceVector) {}
   void Execute() override {
     // TODO
+    std::string temp = sourceVector->at(index1);
+    sourceVector->at(index1) = sourceVector->at(index2);
+    sourceVector->at(index2) = temp;
   }
 };
 
