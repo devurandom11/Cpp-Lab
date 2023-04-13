fd5c0abba3eeefb6bcceec854494a981f692ed8a
Mike Crawford <dev.random.87@gmail.com>
2023-04-12 22:35:25 -0700

    Try memoization

--- DSA-1/LAB BST validity checker/BSTChecker.h
+++ DSA-1/LAB BST validity checker/BSTChecker.h
@@ -3,34 +3,44 @@
 
 #include <iostream>
 #include <limits>
+#include <unordered_set>
 
 #include "Node.h"
 
 class BSTChecker {
  private:
-  // Declare constexpr variables for maximum and minimum int values
+  // Convenience constants
   static constexpr int MAX = std::numeric_limits<int>::max();
   static constexpr int MIN = std::numeric_limits<int>::min();
 
-  static Node* DFSHelper(Node* node, int minKey, int maxKey) {
-    // Perform a DFS traversal of the tree, checking the validity of each node
+  static Node* DFSHelper(Node* node, int minKey, int maxKey,
+                         std::unordered_set<Node*>& visited) {
     if (!node) {
       return nullptr;
     }
+
+    // Check if the node has already been visited (ancestor detection)
+    if (visited.find(node) != visited.end()) {
+      return node;
+    }
+    visited.insert(node);
 
     if (node->key < minKey || node->key > maxKey) {
       return node;
     }
 
-    Node* leftOffender = DFSHelper(node->left, minKey, node->key - 1);
-    Node* rightOffender = DFSHelper(node->right, node->key + 1, maxKey);
+    Node* leftOffender = DFSHelper(node->left, minKey, node->key - 1, visited);
+    Node* rightOffender =
+        DFSHelper(node->right, node->key + 1, maxKey, visited);
 
     return leftOffender ? leftOffender : rightOffender;
   }
 
  public:
   static Node* CheckBSTValidity(Node* root) {
-    return DFSHelper(root, MIN, MAX);
+    // Pass in a set for memoization
+    std::unordered_set<Node*> visited;
+    return DFSHelper(root, MIN, MAX, visited);
   }
 };
 
