66a83ce3219e1ae1ce0f03c48c256f73b2d6d1e2
Mike Crawford <dev.random.87@gmail.com>
2023-04-12 23:18:31 -0700

    Working for all but 1 test

--- DSA-1/LAB BST validity checker/BSTChecker.h
+++ DSA-1/LAB BST validity checker/BSTChecker.h
@@ -1,53 +1,51 @@
 #ifndef BSTCHECKER_H
 #define BSTCHECKER_H
 
+#include <algorithm>
 #include <iostream>
 #include <limits>
 #include <unordered_set>
+#include <vector>
 
 #include "Node.h"
 
 class BSTChecker {
  private:
-  // Convenience constants
-  static constexpr int MAX = std::numeric_limits<int>::max();
-  static constexpr int MIN = std::numeric_limits<int>::min();
-
-  static Node* DFSHelper(Node* node, int minKey, int maxKey,
-                         std::unordered_set<Node*>& visited, Node* parent) {
-    if (!node) {
-      return nullptr;
-    }
-
-    // Check if the node has already been visited (ancestor detection)
-    if (visited.find(node) != visited.end()) {
+  // Static helper method
+  static Node* LastTryHelper(Node* node, int minKey, int maxKey,
+                             std::vector<Node*>& ancestors) {
+    // Base cases
+    if (!node) return nullptr;
+    if (node->key < minKey || node->key > maxKey) return node;
+    if (std::find(ancestors.begin(), ancestors.end(), node->left) !=
+            ancestors.end() ||
+        std::find(ancestors.begin(), ancestors.end(), node->right) !=
+            ancestors.end())
       return node;
-    }
-    visited.insert(node);
 
-    if (node->key < minKey || node->key > maxKey) {
-      return node;
-    }
-
-    // Check if left or right child is pointing to the parent
-    if ((node->left && node->left == parent) ||
-        (node->right && node->right == parent)) {
-      return node;
-    }
-
+    // Recursive case
+    // Add to set of ancestors
+    ancestors.push_back(node);
     Node* leftOffender =
-        DFSHelper(node->left, minKey, node->key - 1, visited, node);
-    Node* rightOffender =
-        DFSHelper(node->right, node->key + 1, maxKey, visited, node);
+        LastTryHelper(node->left, minKey, node->key - 1, ancestors);
+    if (leftOffender) return leftOffender;
 
-    return leftOffender ? leftOffender : rightOffender;
+    Node* rightOffender =
+        LastTryHelper(node->right, node->key + 1, maxKey, ancestors);
+
+    // Remove from set of ancestors
+    ancestors.pop_back();
+
+    // Return
+    return rightOffender ? rightOffender : leftOffender;
   }
 
  public:
+  // Check if a binary search tree is valid
   static Node* CheckBSTValidity(Node* root) {
-    // Pass in a set for memoization
-    std::unordered_set<Node*> visited;
-    return DFSHelper(root, MIN, MAX, visited, nullptr);
+    std::vector<Node*> ancestors;
+    return LastTryHelper(root, std::numeric_limits<int>::min(),
+                         std::numeric_limits<int>::max(), ancestors);
   }
 };
 
