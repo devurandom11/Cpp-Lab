4aecfc765c9fc829cc5f66595d95ebfbea89f849
Mike Crawford <dev.random.87@gmail.com>
2023-05-02 23:55:49 -0700

    Initial starting point

---
+++ DSA-1/Sets/StaticSet.h
@@ -0,0 +1,89 @@
+#ifndef STATICSET_H
+#define STATICSET_H
+
+#include <functional>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+template <typename T>
+class StaticSet {
+ private:
+  std::unordered_set<T> set;
+
+ public:
+  // Constructs an empty StaticSet
+  StaticSet() {}
+
+  // Constructs the StaticSet by adding all vector items
+  StaticSet(const std::vector<T>& items) {
+    for (const T& item : items) {
+      set.insert(item);
+    }
+  }
+
+  // Constructs the StaticSet by copying itemsToCopy's items
+  StaticSet(const std::unordered_set<T>& itemsToCopy) : set(itemsToCopy) {}
+
+  virtual ~StaticSet() {}
+
+  bool Contains(const T& item) const { return 1 == set.count(item); }
+
+  int GetSize() const { return (int)set.size(); }
+
+  void Print(std::ostream& output, std::string separator,
+             std::string prefix = "", std::string suffix = "") const {
+    output << prefix;
+    bool firstItem = true;
+    for (const T& item : set) {
+      if (firstItem) {
+        output << item;
+        firstItem = false;
+      } else {
+        output << separator << item;
+      }
+    }
+    output << suffix;
+  }
+
+  // Returns a StaticSet<T> containing each element from this set that is not
+  // in otherSet.
+  StaticSet<T> Difference(const StaticSet& otherSet) const {
+    // Your code here (remove placeholder line below)
+    return StaticSet<T>();
+  }
+
+  // Returns a StaticSet<T> containing each element from this set that
+  // satisfies the predicate.
+  // - If predicate(item) returns true, item satisfies the predicate.
+  // - If predicate(item) returns false, item does not satisfy the predicate.
+  StaticSet<T> Filter(std::function<bool(const T&)> predicate) const {
+    // Your code here (remove placeholder line below)
+    return StaticSet<T>();
+  }
+
+  // Returns a StaticSet<T> containing each element from this set that is also
+  // in otherSet.
+  StaticSet<T> Intersection(const StaticSet& otherSet) const {
+    // Your code here (remove placeholder line below)
+    return StaticSet<T>();
+  }
+
+  // Calls mapFunction(item) for each item in this set and adds the returned
+  // item to a StaticSet<U>. After mapFunction has been called for each item
+  // in this set, the StaticSet<U> object is returned.
+  template <typename U>
+  StaticSet<U> Map(std::function<U(const T&)> mapFunction) const {
+    // Your code here (remove placeholder line below)
+    return StaticSet<U>();
+  }
+
+  // Returns a StaticSet<T> containing every element from this set and every
+  // element from otherSet.
+  StaticSet<T> Union(const StaticSet& otherSet) const {
+    // Your code here (remove placeholder line below)
+    return StaticSet<T>();
+  }
+};
+
+#endif
---
+++ DSA-1/Sets/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+#include "StaticSetTestCases.h"
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  // First test case tests binary operations: union, intersection, and
+  // difference
+  BinaryOpsTestCase<int> binaryOpsTestCase(
+      {42, 63, 99, 32, 18, 77, 64, 50, 12}, {64, 16, 32, 8, 4, 1, 2},
+      {1, 2, 4, 8, 12, 16, 18, 32, 42, 50, 63, 64, 77, 99},  // expected union
+      {32, 64},                      // expected intersection
+      {42, 63, 99, 18, 77, 50, 12},  // expected A minus B
+      {1, 2, 4, 8, 16}               // expected B minus A
+  );
+  int binaryTestCasePoints = binaryOpsTestCase.Execute(cout);
+  cout << endl;
+
+  // Next test case tests unary operations: filter and map
+  UnaryOpsTestCase<string, int> unaryOpsTestCase(
+      {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+       "nine", "ten"},
+      [&](const string& str) {
+        // Keep each string with a length <= 4
+        return (str.length() <= 4);
+      },
+      [&](const string& str) {
+        // Map from the string to the string's length
+        return (int)str.length();
+      },
+      {"zero", "one", "two", "four", "five", "six", "nine",
+       "ten"},   // expected filtered
+      {3, 4, 5}  // expected mapped
+  );
+  int unaryTestCasePoints = unaryOpsTestCase.Execute(cout);
+
+  cout << endl;
+  cout << "Binary operations score: " << binaryTestCasePoints << " out of 3";
+  cout << endl;
+  cout << "Unary operations score: " << unaryTestCasePoints << " out of 2";
+  cout << endl;
+
+  return 0;
+}
